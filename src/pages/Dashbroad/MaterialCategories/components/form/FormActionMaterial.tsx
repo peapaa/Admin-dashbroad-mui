70805f8e4a12e9f3e8c77f9e0f5066f5d4b3b32a
thiendd <thienqxa99@gmail.com>
2024-09-12 13:43:37 +0700

    update form material action

--- src/pages/Dashbroad/MaterialCategories/components/form/FormActionMaterial.tsx
+++ src/pages/Dashbroad/MaterialCategories/components/form/FormActionMaterial.tsx
@@ -7,6 +7,7 @@ import ControllerFormInput from "@/pages/Dashbroad/MaterialCategories/components
 import ControllerFormSelectWithCategories from "@/pages/Dashbroad/MaterialCategories/components/form/ControllerFormSelectWithCategories";
 import ControllerFormSelectWithSupplier from "@/pages/Dashbroad/MaterialCategories/components/form/ControllerFormSelectWithSupplier";
 import { FormActionMateriaProps } from "@/pages/Dashbroad/MaterialCategories/type";
+import { Grid } from "@mui/material";
 
 const FormActionMaterial: React.FC<FormActionMateriaProps> = ({
   onSubmit,
@@ -22,7 +23,7 @@ const FormActionMaterial: React.FC<FormActionMateriaProps> = ({
   return (
     <div className="bg-white w-full rounded-md p-5 ">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex gap-10 my-10 h-[400px] items-center justify-center">
+        <div className="flex gap-10 my-20 h-[400px] items-center justify-center">
           {/* image required */}
           <Controller
             control={control}
@@ -39,76 +40,85 @@ const FormActionMaterial: React.FC<FormActionMateriaProps> = ({
             }}
           />
 
-          <div className="flex flex-col items-center justify-center gap-5">
-            <div className=" flex gap-5 items-center justify-center shadow-shadowCategory px-8 py-10 rounded-md">
-              {/* part_number required */}
-              <div className="flex flex-col">
-                <ControllerFormInput
-                  control={control}
-                  name="part_number"
-                  errors={errors}
-                  title="Part number"
-                  typeInput="string"
-                />
-                {/* name don't required */}
-                <ControllerFormInput
-                  control={control}
-                  name="name"
-                  errors={errors}
-                  title="Name"
-                  typeInput="string"
-                />
-                {/* type don't required */}
+          <div className="w-full max-w-[660px] shadow-shadowCategory px-8 py-10 rounded-md">
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                {/* part_number required */}
+                <div className="flex gap-5  w-full">
+                  <ControllerFormInput
+                    control={control}
+                    name="part_number"
+                    errors={errors}
+                    title="Part number"
+                    typeInput="string"
+                  />
+                  {/* large_title required */}
+                  <ControllerFormInput
+                    control={control}
+                    name="large_title"
+                    errors={errors}
+                    title="Large title"
+                    typeInput="string"
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <div className="flex gap-5  w-full">
+                  {/* type don't required */}
+                  <ControllerFormInput
+                    control={control}
+                    name="type"
+                    errors={errors}
+                    title="Type"
+                    typeInput="number"
+                  />
 
-                <ControllerFormInput
-                  control={control}
-                  name="type"
-                  errors={errors}
-                  title="Type"
-                  typeInput="number"
-                />
+                  {/* name don't required */}
+                  <ControllerFormInput
+                    control={control}
+                    name="name"
+                    errors={errors}
+                    title="Name"
+                    typeInput="string"
+                  />
+                </div>
+              </Grid>
 
-                {/* large_title required */}
-
-                <ControllerFormInput
-                  control={control}
-                  name="large_title"
-                  errors={errors}
-                  title="Large title"
-                  typeInput="string"
-                />
-              </div>
-
-              <div className="flex flex-col">
-                {/* small_title  required */}
-                <ControllerFormInput
-                  control={control}
-                  name="small_title"
-                  errors={errors}
-                  title="Small title"
-                  typeInput="string"
-                />
-                {/*basic_price required */}
-                <ControllerFormInput
-                  control={control}
-                  name="basic_price"
-                  errors={errors}
-                  title="Basic price"
-                  typeInput="number"
-                />
-                {/* category required */}
-                <ControllerFormSelectWithCategories
-                  control={control}
-                  errorForm={errors}
-                />
-                {/* supplier  required */}
-                <ControllerFormSelectWithSupplier
-                  control={control}
-                  errorForm={errors}
-                />
-              </div>
-              {/* basic_price don't required */}
-            </div>
+              <Grid item xs={12}>
+                <div className="flex gap-5 w-full">
+                  {/* small_title  required */}
+                  <ControllerFormInput
+                    control={control}
+                    name="small_title"
+                    errors={errors}
+                    title="Small title"
+                    typeInput="string"
+                  />
+                  {/* basic_price don't required */}
+                  <ControllerFormInput
+                    control={control}
+                    name="basic_price"
+                    errors={errors}
+                    title="Basic price"
+                    typeInput="number"
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <div className="flex gap-5 w-full">
+                  {/* category required */}
+                  <ControllerFormSelectWithCategories
+                    control={control}
+                    errorForm={errors}
+                  />
+                  {/* supplier  required */}
+                  <ControllerFormSelectWithSupplier
+                    control={control}
+                    errorForm={errors}
+                  />
+                </div>
+              </Grid>
+            </Grid>
           </div>
         </div>
         <ButtonForm loading={loading} />
