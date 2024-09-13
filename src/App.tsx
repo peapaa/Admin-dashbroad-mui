6eedd453936b33a1b1fb001e2a7a4397597be9ac
thiendd <thienqxa99@gmail.com>
2024-09-13 15:44:55 +0700

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,11 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import ProtectedRouter from "./components/Home/ProtectedRouter";
 
 //page
-import Address from "@/pages/Dashbroad/Address/Address";
 import Categories from "@/pages/Dashbroad/Categories/Categories";
 import Dashbroad from "@/pages/Dashbroad/Dashbroad";
 import Main from "@/pages/Dashbroad/Main/Main";
 import MarterialCategories from "@/pages/Dashbroad/MaterialCategories/MarterialCategories";
-import Posts from "@/pages/Dashbroad/Posts/Posts";
-import Purchars from "@/pages/Dashbroad/Purchars/Purchars";
-import Roles from "@/pages/Dashbroad/Roles/Roles";
 import Tag from "@/pages/Dashbroad/Tags/Tag";
-import UserInsight from "@/pages/Dashbroad/UserInsight/UserInsight";
-import Content from "@/pages/Dashbroad/UserManager/Content";
 import Login from "@/pages/Registration/Login";
 
 function App() {
@@ -38,54 +32,6 @@ function App() {
               element={
                 <ProtectedRouter>
                   <Main />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="dashbroad/user-insights"
-              element={
-                <ProtectedRouter>
-                  <UserInsight />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="resources/users"
-              element={
-                <ProtectedRouter>
-                  <Content />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="resources/addresses"
-              element={
-                <ProtectedRouter>
-                  <Address />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="resources/posts"
-              element={
-                <ProtectedRouter>
-                  <Posts />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="resources/purchases"
-              element={
-                <ProtectedRouter>
-                  <Purchars />
-                </ProtectedRouter>
-              }
-            />
-            <Route
-              path="resources/roles"
-              element={
-                <ProtectedRouter>
-                  <Roles />
                 </ProtectedRouter>
               }
             />
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,18 +1,10 @@
 import { HeadCell } from "@/pages/Dashbroad/Categories/type";
 
 // dashboard list
-export const dashboardList = [
-  { href: "dashbroad/main", title: "Main" },
-  { href: "dashbroad/user-insights", title: "User Insights" },
-];
+export const dashboardList = [{ href: "dashbroad/main", title: "Main" }];
 // resources list
 export const resourcesList = [
-  { href: "resources/addresses", title: "Addresses" },
-  { href: "resources/posts", title: "Posts" },
-  { href: "resources/purchases", title: "Purchases" },
-  { href: "resources/roles", title: "Roles" },
   { href: "resources/tags", title: "Tags" },
-  { href: "resources/users", title: "Users" },
   { href: "resources/categories", title: "Categories" },
   { href: "resources/material-categories", title: "Material Categories" },
 ];
